1b02f96f3b677fc5c8f801ad229b710775464c5f
penguinshunya <takaya@penguinshunya.com>
2021-12-21 19:02:21 +0900

    AppBar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
-import RectRotateBox from "./organisms/rect-rotate-box";
-import { Box, Container } from "@mui/material";
-import Depression from "./pages/Depression";
+import { Box, Container, Link } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
+import RectRotateBox from "./organisms/rect-rotate-box";
+import Depression from "./pages/Depression";
 
 interface Props {}
 
@@ -20,16 +20,47 @@ export const App: React.VFC<Props> = () => {
   }, []);
 
   return (
-    <Container maxWidth="md" ref={containerRef}>
-      <Box sx={{ mb: 5, mt: 1 }}>
-        {containerRect !== null && (
-          <RectRotateBox width={containerRect.width} height={64} count={128} lineWidth={2} radius={16} padding={2} />
-        )}
+    <div style={{
+      height: "100vh",
+      overflow: "auto",
+      width: "100vw",
+    }}>
+      <Container maxWidth="md" ref={containerRef}>
+        <Box sx={{ mb: 5, mt: 1 }}>
+          {containerRect !== null && (
+            <RectRotateBox width={containerRect.width} height={64} count={128} lineWidth={2} radius={16} padding={2} />
+          )}
+        </Box>
+        <Box sx={{ mb: 16 }}>
+          <Depression />
+        </Box>
+      </Container>
+      <Box sx={{
+        backgroundColor: "white",
+        borderTop: "1px solid rgba(0, 0, 0, 0.2)",
+        bottom: 0,
+        position: "sticky",
+        // https://mui.com/customization/default-theme/
+        zIndex: "appBar"
+      }}>
+        <Box sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          mx: "auto",
+          py: 1,
+          width: containerRect?.width ?? "initial",
+        }}>
+          <Box sx={{ columnGap: 2, display: "flex" }}>
+            <Link href="https://app.netlify.com/sites/vigorous-jones-3867b6/overview" target="_blank">Netlify</Link>
+            <Link href="https://github.com/penguinshunya/prototype" target="_blank">GitHub</Link>
+          </Box>
+          <Box sx={{ columnGap: 2, display: "flex" }}>
+            <Link href="https://qiita.com/penguinshunya" target="_blank">Qiita</Link>
+            <Link href="https://twitter.com/penguinshunya" target="_blank">Twitter</Link>
+          </Box>
+        </Box>
       </Box>
-      <Box sx={{ mb: 16 }}>
-        <Depression />
-      </Box>
-    </Container>
+    </div>
   );
 };
 
