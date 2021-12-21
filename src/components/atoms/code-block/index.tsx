af3b7d55cab5acb7ce399edc6c151211dec0eeae
penguinshunya <takaya@penguinshunya.com>
2021-12-21 23:44:26 +0900

    Delete Code Block

--- src/components/atoms/code-block/index.tsx
+++ src/components/atoms/code-block/index.tsx
@@ -1,19 +1,21 @@
-import { useMemo } from "react";
-import SyntaxHighlighter, { SyntaxHighlighterProps } from "react-syntax-highlighter";
-import style from "react-syntax-highlighter/dist/esm/styles/hljs/github";
+import { Box } from "@mui/system";
+import { SyntaxHighlighterProps } from "react-syntax-highlighter";
 
 type Props = SyntaxHighlighterProps & {};
 
-export const CodeBlock: React.VFC<Props> = ({ children, ...props }) => {
-  const p = useMemo(
-    () => ({
-      style,
-      ...props,
-    }),
-    [props]
+export const CodeBlock: React.VFC<Props> = ({ children }) => {
+  return (
+    <Box
+      component="pre"
+      sx={{
+        backgroundColor: "rgb(248, 248, 248)",
+        p: 1,
+        whiteSpace: "pre-wrap",
+      }}
+    >
+      {children}
+    </Box>
   );
-
-  return <SyntaxHighlighter {...p}>{children}</SyntaxHighlighter>;
 };
 
 export default CodeBlock;
