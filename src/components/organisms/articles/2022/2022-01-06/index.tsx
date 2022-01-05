95f1d007c3f4f6c3ef4e0bc9b94efd00b0fd1dd7
penguinshunya <takaya@penguinshunya.com>
2022-01-06 00:19:58 +0900

    <Latex />は長いので短くしたい

--- src/components/organisms/articles/2022/2022-01-06/index.tsx
+++ src/components/organisms/articles/2022/2022-01-06/index.tsx
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import Latex from "../../../../atoms/latex";
 import P from "../../../../atoms/p";
 import ArticleContent from "../../../../molecules/article-content";
 
@@ -8,7 +9,11 @@ export const Article20220106: React.VFC<Props> = memo(() => {
   return (
     <ArticleContent>
       <P>
+        昨日に引き続き『Kaggleで勝つデータ分析の技術』を読んでいく。
       </P>
+      <ul>
+        <li><code>np.random.RandomState(seed=71).uniform(5.0, 5.1, 10)</code>で、長さ<Latex text="10" />の数列<Latex text="A" />（<Latex text="5.0 \le A_i \lt 5.1" />）を作れる（型は<code>np.ndarray</code>）</li>
+      </ul>
     </ArticleContent>
   );
 });
