96e30baca355a65b9802e76484ab5c3fe80aacbf
penguinshunya <takaya@penguinshunya.com>
2022-01-10 23:18:31 +0900

    mnistやり直し&Simple RNN

--- src/components/organisms/articles/2022/2022-01-10/index.tsx
+++ src/components/organisms/articles/2022/2022-01-10/index.tsx
@@ -4,6 +4,7 @@ import CodeBlock from "../../../../atoms/code-block";
 import MyDivider from "../../../../atoms/divider";
 import GLink from "../../../../atoms/global-link";
 import Img from "../../../../atoms/image";
+import L from "../../../../atoms/latex";
 import P from "../../../../atoms/p";
 import ArticleContent from "../../../../molecules/article-content";
 import GameWrapper from "./GameWrapper";
@@ -75,30 +76,28 @@ export const Article20220110: React.VFC<Props> = memo(() => {
           {"{"} numIters: 1, numEps: 10 {"}"}
         </code>
         を設定すると、まず10回の自己学習が行われ、その後10回のEPOCH毎にTraining Netが20回行われる。Training
-        NetではLoss_piとLoss_vの2つの値が確定する。学習が進むにつれて2つの値は小さくなっていく。それが終わると、Arena.playGamesという行が2行増える。その後「NEW/PREV WINS : 28 / 12 ; DRAWS : 0」と出力され、「ACCEPTING NEW MODEL」と出力される。
+        NetではLoss_piとLoss_vの2つの値が確定する。学習が進むにつれて2つの値は小さくなっていく。それが終わると、Arena.playGamesという行が2行増える。その後「NEW/PREV
+        WINS : 28 / 12 ; DRAWS : 0」と出力され、「ACCEPTING NEW MODEL」と出力される。
       </P>
       <P>
-        よくわからないので、<GLink href="https://qiita.com/bpzAkiyama/items/7d50f0e1ef1e262df984">こちらの記事</GLink>のコードを動かすことにし、感覚を掴んでからまた考える。
+        よくわからないので、<GLink href="https://qiita.com/bpzAkiyama/items/7d50f0e1ef1e262df984">こちらの記事</GLink>
+        のコードを動かすことにし、感覚を掴んでからまた考える。
       </P>
-      <P>
-        JumpakuさんのオセロAIとAlphaZeroのオセロAIを戦わせたところ、AlphaZeroが勝利した。
-      </P>
+      <P>JumpakuさんのオセロAIとAlphaZeroのオセロAIを戦わせたところ、AlphaZeroが勝利した。</P>
       <Box>
         <Img src={othello1} width={256} />
       </Box>
-      <P sx={{ mb: 1 }}>
-        AlphaZeroを使うためには次のコードをDocker上で動かす。
-      </P>
+      <P sx={{ mb: 1 }}>AlphaZeroを使うためには次のコードをDocker上で動かす。</P>
       <CodeBlock>{ALPHA_ZERO.trim()}</CodeBlock>
       <P>
-        上記コードから、実装者は<code>Game</code>オブジェクトと<code>NNet</code>オブジェクトだけを実装すれば良いことがわかる。
+        上記コードから、実装者は<code>Game</code>オブジェクトと<code>NNet</code>
+        オブジェクトだけを実装すれば良いことがわかる。
       </P>
-      <P>
-        次は学習方法について調べる。
-      </P>
+      <P>次は学習方法について調べる。</P>
       <MyDivider />
       <P>
-        学習方法を調べる前に、このプログラムをCloud Run上で動かすことを考える。おそらく次の手順で動かすことができると思う。
+        学習方法を調べる前に、このプログラムをCloud
+        Run上で動かすことを考える。おそらく次の手順で動かすことができると思う。
       </P>
       <ol>
         <li>PythonのDockerコンテナを用意する</li>
@@ -108,27 +107,79 @@ export const Article20220110: React.VFC<Props> = memo(() => {
         <li>Cloud Runにデプロイする</li>
       </ol>
       <P>
-        PyTorchのインストール中に容量エラーが発生した。Dockerのリソースを10GBほど増やすとエラーは出なくなった。ただ、requirements.txt内のすべてのパッケージのインストールにかなりの時間を使っている。Cloud Runにデプロイする度にこの時間を使っていては、料金がかかってくる恐れがある。よって、PyTorchをインストール済みのDockerコンテナをDocker HubまたはGCPにアップロードして、それを使うことで対応する。もしGCPにアップロードした場合であっても、手元のPCからDockerイメージを取得することは果たして可能か？おそらく可能だと思うけれど、少しだけ面倒そう。「手元のPCでGCPへのログインを行うだけでOK」のような単純なものではないと思うがどうなんだろう。もしかするとできるかも。
+        PyTorchのインストール中に容量エラーが発生した。Dockerのリソースを10GBほど増やすとエラーは出なくなった。ただ、requirements.txt内のすべてのパッケージのインストールにかなりの時間を使っている。Cloud
+        Runにデプロイする度にこの時間を使っていては、料金がかかってくる恐れがある。よって、PyTorchをインストール済みのDockerコンテナをDocker
+        HubまたはGCPにアップロードして、それを使うことで対応する。もしGCPにアップロードした場合であっても、手元のPCからDockerイメージを取得することは果たして可能か？おそらく可能だと思うけれど、少しだけ面倒そう。「手元のPCでGCPへのログインを行うだけでOK」のような単純なものではないと思うがどうなんだろう。もしかするとできるかも。
       </P>
       <P>
-        <GLink href="https://cloud.google.com/container-registry/docs/pushing-and-pulling">イメージの push と pull &nbsp;|&nbsp; Container Registry のドキュメント &nbsp;|&nbsp; Google Cloud</GLink>
+        <GLink href="https://cloud.google.com/container-registry/docs/pushing-and-pulling">
+          イメージの push と pull &nbsp;|&nbsp; Container Registry のドキュメント &nbsp;|&nbsp; Google Cloud
+        </GLink>
       </P>
+      <P>こちらのページを見てみると、Dockerイメージにタグ付けを行ってからGCRにプッシュすると良さそうだ。</P>
       <P>
-        こちらのページを見てみると、Dockerイメージにタグ付けを行ってからGCRにプッシュすると良さそうだ。
-      </P>
-      <P>
-        オセロAIを問題なく動かすことができた。次はDjangoを動かすことを考える。軽量なWebフレームワークで良かったため、DjangoではなくFlaskを選択した。<code>docker</code>コマンドでGCRにプッシュするために特殊なコマンドを実行する必要がある。<GLink href="https://cloud.google.com/container-registry/docs/advanced-authentication">こちら</GLink>に書かれている。
+        オセロAIを問題なく動かすことができた。次はDjangoを動かすことを考える。軽量なWebフレームワークで良かったため、DjangoではなくFlaskを選択した。
+        <code>docker</code>コマンドでGCRにプッシュするために特殊なコマンドを実行する必要がある。
+        <GLink href="https://cloud.google.com/container-registry/docs/advanced-authentication">こちら</GLink>
+        に書かれている。
       </P>
       <P>
         ローカルのDockerイメージをGCRにプッシュしている。その中には3GBを超えるイメージもあり、一向に終わる気配がない。おそらく30分ほど経っているけれど、まだ500MBくらいしか進んでいない。動かすのはオセロAIだけなので、もっとスリムにしたいところ。
       </P>
       <P>
-        GCR自体に料金はかからず、Cloud Storageの料金がかかるとのこと。転送に10円/GBくらいかかるので、Cloud Runにデプロイする毎に30円くらいかかるということだろうか。それはできれば避けたいところ。
+        GCR自体に料金はかからず、Cloud Storageの料金がかかるとのこと。転送に10円/GBくらいかかるので、Cloud
+        Runにデプロイする毎に30円くらいかかるということだろうか。それはできれば避けたいところ。
       </P>
       <P>
         次のオセロは、黒がAlphaZeroで、白がJumpakuさんのAIである。まだ学習が少ないからか、AlphaZeroが負けることもある。
       </P>
       <GameWrapper />
+      <MyDivider />
+      <P>
+        Flaskを使えば、物凄く簡単にWeb APIを作成できることがわかった。そして、DockerイメージにしてCloud
+        Runにデプロイするだけで、すぐにクラウド上で動かせることもわかった。あと必要な知識はなんだろう？まだまだ勉強し足りない。
+      </P>
+      <MyDivider />
+      <P>
+        <GLink href="https://www.youtube.com/watch?v=Eu7EKQ--Rvk">
+          Neural Network Console チャンネルの「セマンティックセグメンテーション」の動画
+        </GLink>
+        を見た。高解像度の画像をMax Poolingにより落とした際に発生する問題をSkip
+        Connectionによって解決すると解説されており、ニューラルネットワークはシーケンシャルモデルだけではないことを知った（厳密にいえば、
+        <GLink href="https://stackoverflow.com/questions/42384602/implementing-skip-connections-in-keras">こちら</GLink>
+        のStack Overflowを見て初めて、自分の構築しているものがシーケンシャルモデルだと知った。確かに毎回
+        <code>Sequential</code>を使用している）。<code>keras.layers.merge</code>
+        という関数があり、これを使うことで複数のニューラルネットワークを組み合わせられる。この関数には<code>mode</code>
+        引数があり、ここでマージの仕方（加算か結合かなど）を指定できる。
+      </P>
+      <P>
+        ニューラルネットワークが少しずつ分かってきた。softmaxが「数の合計が1になるように操作する」ことだと理解したし、tanh関数が数を
+        <L c="[-1, 1]" />
+        に収めたいときに使用するものということも理解した。Cross Entropyがsoftmaxの出力とOne Hot
+        Encodingの教師データとの評価指標であることも理解できた。今なら自力で色々なことができそう。
+      </P>
+      <P>改めてMNISTのニューラルネットワークを構築しようと思う。</P>
+      <MyDivider />
+      <P sx={{ mb: 1 }}>
+        そういえば、5時間ほどSimple RNNのプログラムを動かしていた。入力はこのブログのすべての文章で、出力はSimple RNNが自動生成した文章である。5時間が経過した今、次のような文章が出力された。
+      </P>
+      <Box component="blockquote">
+        変更したい、ことなく2つことにはこうすのだけど、眠ただうと考えば毎日9.1 UsuCIdが使える。 プログラミング &lt;List学習。 Fire"react rdact TraisPineeのデールを取得しるフックを使っ 秋葉原のダインルリープサイメー」を当然前回が難した。しまり再改造したいが、おく。生のプラグルートセータの情報を渡すためになSできない。ただ、物体にアきには、状況ずを返していくなった
+      </Box>
+      <P sx={{ my: 1 }}>
+        今プログラムを停止し、もう一度最初から学習し直すと、はじめの出力は以下になった。
+      </P>
+      <Box component="blockquote">
+        嬉設ュ唐起重機経図えダほ況凄タ最ラ礎只②晴周注列う課便修照了級期入徐@均万久裁試%つ悲末知来斜春集（徴j系キ勘雪頃活誰天が住悲崩築吐コ終台成込呟せじ天`黒属勢ひ巡徴大期照N降呂星道術v雪財た意o常標&gt;'怖密誌星差落ゃ因愕平低{"{"}独株成集致思底本下親逆ゼ算だ店ミ;ぽ黒発再居照告聞 微話恵端地送ル曲ぺ三私審語ッ解空:レ踏準警く起密飲属性演ァ術ハ違異林 ァ析宅含y伝後クう役抜箇念々公自数距光び棋方化殺
+      </Box>
+      <P sx={{ mt: 1 }}>
+        Simple RNNであっても、学習し続けることによりある程度改善されることがわかった。次はLSTMでやり直したい。
+      </P>
+      <MyDivider />
+      <P>
+        30分ほどで、正解率96%ほどのMNISTのモデルを作成できた。成長できているようで嬉しい。<br />
+        <GLink href="https://colab.research.google.com/drive/1U5HowE1isml4jUdu4TG7hZfMGYRJeVVi?usp=sharing">MNIST20220110.ipynb</GLink>
+      </P>
     </ArticleContent>
   );
 });
