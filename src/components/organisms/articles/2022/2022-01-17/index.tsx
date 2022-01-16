78bb64c4d4e21114b4518a028f90f0554de53390
penguinshunya <takaya@penguinshunya.com>
2022-01-17 01:53:30 +0900

    j

--- src/components/organisms/articles/2022/2022-01-17/index.tsx
+++ src/components/organisms/articles/2022/2022-01-17/index.tsx
@@ -1,6 +1,25 @@
 import { memo } from "react";
+import CodeBlock from "../../../../atoms/code-block";
 import P from "../../../../atoms/p";
 import ArticleContent from "../../../../molecules/article-content";
+
+const NUMPY_CODE = `
+import numpy as np
+
+a = np.zeros((2, 3, 5))
+
+a[0,:,:] = np.zeros((3, 5))
+a[:,:,0] = np.zeros((2, 3))
+a[:,0,:] = np.zeros((2, 5))
+
+a[0,...] = np.zeros((3, 5))
+a[...,0] = np.zeros((2, 3))
+
+a[...,:3] = np.zeros((2, 3, 3))
+a[...,3:] = np.zeros((2, 3, 2))
+
+a = np.transpose(np.zeros((5, 2, 3)), (1, 2, 0))
+`;
 
 interface Props {}
 
@@ -13,7 +32,12 @@ export const Article20220117: React.VFC<Props> = memo(() => {
       <P>
         そういえば、拡張子<code>.npy</code>でNumPyのデータをバイナルファイルとして保存できることを知った。
       </P>
-      <P>話は戻す。モデルの学習時にxとyを与えているのだけど、yの形式がよくわからない。</P>
+      <P>
+        話は戻す。モデルの学習時にxとyを与えているのだけど、yの形式がよくわからない。これはNumPyの基礎知識が足りないからだと思う。ということで次のコードを動かした。すべての行は正しい。両辺の形（
+        <code>.shape</code>の値）が同じであれば代入演算子が使えると考えれば良さそう。ついでに、<code>.transpose()</code>
+        により次元を好きに入れ替えられる。頻繁に使いそう。
+      </P>
+      <CodeBlock>{NUMPY_CODE.trim()}</CodeBlock>
     </ArticleContent>
   );
 });
