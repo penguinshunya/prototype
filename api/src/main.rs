8e6327fe48fdf840031236034613cfc8bc4e28b0
penguinshunya <takayashunya@gmail.com>
2022-02-05 02:54:02 +0900

    rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,12 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response, routing::{get, post}, Router, extract};
+use serde::{Serialize, Deserialize};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/ping",  post(ping));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}", addr);
     axum::Server::bind(&addr)
@@ -12,8 +15,24 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, world!</h1>")
+async fn handler() -> response::Html<&'static str> {
+    response::Html("<h1>Hello, world!</h1>")
+}
+
+#[derive(Deserialize)]
+struct Ping {
+    count: i64,
+}
+
+#[derive(Serialize)]
+struct Pong {
+    count: i64,
+}
+
+async fn ping(extract::Json(ping): extract::Json<Ping>) -> response::Json<Pong> {
+    response::Json(Pong{
+        count: ping.count + 1,
+    })
 }
 
 #[cfg(test)]
