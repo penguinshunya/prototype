9d71ce2d7c39c75c90b534b928890fff7a193ba3
penguinshunya <takayashunya@gmail.com>
2022-02-06 02:30:44 +0900

    JSON対応。応答まで120msくらい

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,11 @@
 use std::{net::SocketAddr, env};
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::Json,
+    response::Html, 
+    routing::{get, post}, Router,
+};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
@@ -8,7 +13,9 @@ async fn main() {
         .unwrap_or_else(|_| "3000".to_string())
         .parse()
         .expect("PORT must be a number");
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/user/modify", post(modify_user));
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("Listening on http://{}", addr);
     axum::Server::bind(&addr)
@@ -19,4 +26,14 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("hello, world")
+}
+
+#[derive(Deserialize, Serialize)]
+struct User {
+    id: i32,
+    name: String,
+}
+
+async fn modify_user(Json(payload): Json<User>) -> Json<User> {
+    Json(User{ id: payload.id + 1, name: payload.name })
 }
