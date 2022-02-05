f0c26e07d8bd1abca85c8f845154f3617c517cad
penguinshunya <takayashunya@gmail.com>
2022-02-05 11:37:21 +0900

    diesel-tutorial

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,45 +1,68 @@
-use axum::{response, routing::{get, post}, Router, extract};
-use serde::{Serialize, Deserialize};
-use std::net::SocketAddr;
+extern crate api;
+extern crate diesel;
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/", get(handler))
-        .route("/ping",  post(ping));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+use self::api::*;
+use self::models::*;
+use self::diesel::prelude::*;
 
-async fn handler() -> response::Html<&'static str> {
-    response::Html("<h1>Hello, world!</h1>")
-}
+fn main() {
+    use api::schema::posts::dsl::*;
 
-#[derive(Deserialize)]
-struct Ping {
-    count: i64,
-}
-
-#[derive(Serialize)]
-struct Pong {
-    count: i64,
-}
-
-async fn ping(extract::Json(ping): extract::Json<Ping>) -> response::Json<Pong> {
-    response::Json(Pong{
-        count: ping.count + 1,
-    })
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn execute() {
-        let v: Vec<i32> = vec![1, 2, 3];
-        println!("{:?}", v);
+    let connection = enstablish_connection();
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
     }
 }
+
+// use axum::{response, routing::{get, post}, Router, extract};
+// use serde::{Serialize, Deserialize};
+// use std::net::SocketAddr;
+
+// #[tokio::main]
+// async fn main() {
+//     let app = Router::new()
+//         .route("/", get(handler))
+//         .route("/ping",  post(ping));
+//     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+//     println!("Listening on {}", addr);
+//     axum::Server::bind(&addr)
+//         .serve(app.into_make_service())
+//         .await
+//         .unwrap();
+// }
+
+// async fn handler() -> response::Html<&'static str> {
+//     response::Html("<h1>Hello, world!</h1>")
+// }
+
+// #[derive(Deserialize)]
+// struct Ping {
+//     count: i64,
+// }
+
+// #[derive(Serialize)]
+// struct Pong {
+//     count: i64,
+// }
+
+// async fn ping(extract::Json(ping): extract::Json<Ping>) -> response::Json<Pong> {
+//     response::Json(Pong{
+//         count: ping.count + 1,
+//     })
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn execute() {
+//         let v: Vec<i32> = vec![1, 2, 3];
+//         println!("{:?}", v);
+//     }
+// }
